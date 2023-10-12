9227ae7a379a2b52de646e51ffe379d66783d703
Isabel Molina <mariaisabelmolinafer@gmail.com>
2023-10-12 14:17:09 -0300

    Book Store with MERN STACK freeCodeCamp project

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://mariaisabelmolinafer:A6pqGeVPtnaffhIp@books-store-mern.oidv17h.mongodb.net/books-collection?retryWrites=true&w=majority'
